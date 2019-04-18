dd656805221acdd6ec87f35b43fae26fb3d7d59e
Victor Nahuel Chaves <nahuel.chaves@gmail.com>
2019-04-18 10:11:15 -0300

    Nahuel/validate chart api (#8)
    * Add simple call for validating published and draft charts
    
    * rename ChartValidationItem Poco
    
    * Simplify ChartValidation model
    
    * * Rename ChartValidation to ChartValidationResult
    * Bump Version

---
+++ src/test/java/seatsio/charts/ValidateChartTest.java
@@ -0,0 +1,32 @@
+package seatsio.charts;
+
+import org.junit.Test;
+import seatsio.SeatsioClientTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ValidateChartTest extends SeatsioClientTest {
+
+    @Test
+    public void validatePublishedChart() {
+        String chartKey = createTestChartWithErrors();
+
+        ChartValidationResult validationRes = client.charts.validatePublishedVersion(chartKey);
+
+        assertThat(validationRes.errors).contains("VALIDATE_DUPLICATE_LABELS");
+        assertThat(validationRes.errors).contains("VALIDATE_UNLABELED_OBJECTS");
+        assertThat(validationRes.errors).contains("VALIDATE_UNLABELED_OBJECTS");
+    }
+
+    @Test(expected = seatsio.SeatsioException.class)
+    public void validateDraftChart() {
+        String chartKey = createTestChartWithErrors();
+
+
+        client.events.create(chartKey);
+
+        client.charts.update(chartKey, "New name");
+
+        client.charts.validateDraftVersion(chartKey);
+    }
+}
