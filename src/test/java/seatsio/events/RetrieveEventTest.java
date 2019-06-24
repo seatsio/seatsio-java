84d5e0e95219b1c3ad41bc5ab0697431b27c6a34
mroloux <matti@seats.io>
2019-06-24 23:16:28 +0200

    Fixed flaky test

--- src/test/java/seatsio/events/RetrieveEventTest.java
+++ src/test/java/seatsio/events/RetrieveEventTest.java
@@ -25,7 +25,7 @@ public class RetrieveEventTest extends SeatsioClientTest {
         assertThat(retrievedEvent.forSaleConfig).isNull();
         assertThat(retrievedEvent.supportsBestAvailable).isTrue();
         Instant now = Instant.now();
-        assertThat(retrievedEvent.createdOn).isBetween(now.minus(1, MINUTES), now);
+        assertThat(retrievedEvent.createdOn).isBetween(now.minus(1, MINUTES), now.plus(1, MINUTES));
         assertThat(retrievedEvent.updatedOn).isNull();
     }
 }
