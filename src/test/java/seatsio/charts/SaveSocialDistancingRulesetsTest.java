bb627c68eca3b4e4b42703388301206ff2545a95
mroloux <matti@seats.io>
2020-10-08 13:03:33 +0200

    Added tests

--- src/test/java/seatsio/charts/SaveSocialDistancingRulesetsTest.java
+++ src/test/java/seatsio/charts/SaveSocialDistancingRulesetsTest.java
@@ -24,4 +24,30 @@ public class SaveSocialDistancingRulesetsTest extends SeatsioClientTest {
         Chart retrievedChart = client.charts.retrieve(chart.key);
         assertThat(retrievedChart.socialDistancingRulesets).isEqualTo(rulesets);
     }
+
+    @Test
+    public void saveFixedRuleset() {
+        Chart chart = client.charts.create();
+        Map<String, SocialDistancingRuleset> rulesets = ImmutableMap.of(
+                "ruleset1", SocialDistancingRuleset.fixed(0, "My first ruleset", newHashSet("A-1"))
+        );
+
+        client.charts.saveSocialDistancingRulesets(chart.key, rulesets);
+
+        Chart retrievedChart = client.charts.retrieve(chart.key);
+        assertThat(retrievedChart.socialDistancingRulesets).isEqualTo(ImmutableMap.of("ruleset1", new SocialDistancingRuleset(0, "My first ruleset", 0, false, 0, 0, 0, 0, true, newHashSet("A-1"), newHashSet())));
+    }
+
+    @Test
+    public void saveRuleBasedRuleset() {
+        Chart chart = client.charts.create();
+        Map<String, SocialDistancingRuleset> rulesets = ImmutableMap.of(
+                "ruleset1", SocialDistancingRuleset.ruleBased(0, "My first ruleset", 1, true, 2, 1, 10, 0, newHashSet("A-1"), newHashSet("A-2"))
+        );
+
+        client.charts.saveSocialDistancingRulesets(chart.key, rulesets);
+
+        Chart retrievedChart = client.charts.retrieve(chart.key);
+        assertThat(retrievedChart.socialDistancingRulesets).isEqualTo(ImmutableMap.of("ruleset1", new SocialDistancingRuleset(0, "My first ruleset", 1, true, 2, 1, 10, 0, false, newHashSet("A-1"), newHashSet("A-2"))));
+    }
 }
