1c6749d00befea8c108ba62ba70c80827e489811
Ben Verbeken <ben@seats.io>
2021-12-13 17:57:39 +0100

    missing test

--- src/test/java/seatsio/events/ChangeObjectStatusForMultipleEventsTest.java
+++ src/test/java/seatsio/events/ChangeObjectStatusForMultipleEventsTest.java
@@ -1,8 +1,10 @@
 package seatsio.events;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
 import org.junit.jupiter.api.Test;
 import seatsio.SeatsioClientTest;
+import seatsio.SeatsioException;
 import seatsio.charts.SocialDistancingRuleset;
 import seatsio.holdTokens.HoldToken;
 
@@ -12,6 +14,7 @@ import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Sets.newHashSet;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 public class ChangeObjectStatusForMultipleEventsTest extends SeatsioClientTest {
 
@@ -90,6 +93,37 @@ public class ChangeObjectStatusForMultipleEventsTest extends SeatsioClientTest {
 
         assertThat(client.events.retrieveObjectInfo(event1.key, "A-1").status).isEqualTo(EventObjectInfo.BOOKED);
         assertThat(client.events.retrieveObjectInfo(event2.key, "A-1").status).isEqualTo(EventObjectInfo.BOOKED);
+    }
+
+
+    @Test
+    public void allowedPreviousStatuses() {
+        String chartKey = createTestChart();
+        Event event1 = client.events.create(chartKey);
+        Event event2 = client.events.create(chartKey);
+
+        try {
+            client.events.changeObjectStatus(asList(event1.key, event2.key), newArrayList("A-1"), EventObjectInfo.BOOKED, null, null, null, null, null, true, Sets.newHashSet("MustBeThisStatus"), null);
+            fail("expected exception");
+        } catch (SeatsioException e) {
+            assertThat(e.errors).hasSize(1);
+            assertThat(e.errors.get(0).getCode()).isEqualTo("ILLEGAL_STATUS_CHANGE");
+        }
+    }
+
+    @Test
+    public void rejectedPreviousStatuses() {
+        String chartKey = createTestChart();
+        Event event1 = client.events.create(chartKey);
+        Event event2 = client.events.create(chartKey);
+
+        try {
+            client.events.changeObjectStatus(asList(event1.key, event2.key), newArrayList("A-1"), EventObjectInfo.BOOKED, null, null, null, null, null, true, null, Sets.newHashSet("free"));
+            fail("expected exception");
+        } catch (SeatsioException e) {
+            assertThat(e.errors).hasSize(1);
+            assertThat(e.errors.get(0).getCode()).isEqualTo("ILLEGAL_STATUS_CHANGE");
+        }
     }
 
 }
