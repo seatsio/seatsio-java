53e1b7e890dab592ed91295de4ab750781e035af
mroloux <matti@seats.io>
2024-01-17 15:34:13 +0100

    Exposed extra properties for event log items (#162)

--- src/main/java/seatsio/eventLog/EventLogItem.java
+++ src/main/java/seatsio/eventLog/EventLogItem.java
@@ -1,6 +1,13 @@
 package seatsio.eventLog;
 
+import java.time.Instant;
+import java.util.Map;
+
 public class EventLogItem {
 
+    public long id;
     public String type;
+    public String workspaceKey;
+    public Instant date;
+    public Map<?, ?> data;
 }
