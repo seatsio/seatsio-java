107b8c33f0e33c2e986260e1757d2f3a252f4ce3
mroloux <matti@seats.io>
2024-02-12 12:51:46 +0100

    Tweaked event log item format

--- src/main/java/seatsio/eventLog/EventLogItem.java
+++ src/main/java/seatsio/eventLog/EventLogItem.java
@@ -7,7 +7,6 @@ public class EventLogItem {
 
     public long id;
     public String type;
-    public String workspaceKey;
-    public Instant date;
+    public Instant timestamp;
     public Map<?, ?> data;
 }
