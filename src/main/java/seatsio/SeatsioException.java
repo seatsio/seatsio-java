cb6835464281a18e39540e9d0f3aae3e2ca095d2
Ben Verbeken <ben@seats.io>
2024-12-19 17:12:30 +0100

    refactoring

--- src/main/java/seatsio/SeatsioException.java
+++ src/main/java/seatsio/SeatsioException.java
@@ -39,12 +39,16 @@ public class SeatsioException extends RuntimeException {
             SeatsioExceptionTO parsedException = fromJson(responseBody);
             if (response.getStatus() == 429) {
                 return new RateLimitExceededException(parsedException.errors, parsedException.requestId);
-            } else if (parsedException.errors.stream().anyMatch(e -> e.getCode().equals("BEST_AVAILABLE_OBJECTS_NOT_FOUND"))) {
+            } else if (isBestAvailableObjectsNotFound(parsedException)) {
                 return new BestAvailableObjectsNotFoundException(parsedException.errors, parsedException.requestId);
             }
             return new SeatsioException(parsedException.errors, parsedException.requestId);
         }
         return new SeatsioException(request, response);
+    }
+
+    private static boolean isBestAvailableObjectsNotFound(SeatsioExceptionTO parsedException) {
+        return parsedException.errors.stream().anyMatch(e -> "BEST_AVAILABLE_OBJECTS_NOT_FOUND".equals(e.getCode()));
     }
 
     private static String exceptionMessage(List<ApiError> errors) {
