8eda4c014c3162f35c9ea8022a70ea1af30b5a27
Steve Chaloner <schaloner@users.noreply.github.com>
2025-06-17 14:13:24 +0200

    Delete workspaces (#223)
    * Delete workspaces
    
    This PR exposes workspace deletion for inactive workspaces.
    
    * Removed unnecessary test
    
    * Use error code instead of message
    
    ---------
    
    Co-authored-by: Steve Chaloner <steve@seats.io>

---
+++ src/test/java/seatsio/workspaces/DeleteWorkspaceTest.java
@@ -0,0 +1,21 @@
+package seatsio.workspaces;
+
+import org.junit.jupiter.api.Test;
+import seatsio.SeatsioClientTest;
+import seatsio.SeatsioException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DeleteWorkspaceTest extends SeatsioClientTest {
+
+    @Test
+    public void deleteInactiveWorkspace() {
+        Workspace workspace = client.workspaces.create("my workspace");
+        client.workspaces.deactivate(workspace.key);
+        client.workspaces.delete(workspace.key);
+
+        SeatsioException ex = assertThrows(SeatsioException.class, () -> client.workspaces.retrieve(workspace.key));
+        assertThat(ex.errors.get(0).getCode()).isEqualTo("WORKSPACE_NOT_FOUND");
+    }
+}
