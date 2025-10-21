3c6446dedf6d3900f8aca8bccddf7963e513e5ef
Steve Chaloner <steve@seats.io>
2025-10-21 13:23:59 +0200

    Move event to a new chart copy
    Creates a new copy of the chart the event belongs to, and moves the event to that chart.

--- src/main/java/seatsio/events/Events.java
+++ src/main/java/seatsio/events/Events.java
@@ -496,6 +496,14 @@ public class Events {
                 .body(request.build().toString()));
     }
 
+    public Event moveEventToNewChartCopy(String eventKey) {
+        String response = unirest.stringResponse(post(baseUrl + "/events/{key}/actions/move-to-new-chart-copy")
+                .routeParam("key", eventKey));
+        TypeToken<Event> typeToken = new TypeToken<>() {
+        };
+        return gson().fromJson(response, typeToken.getType());
+    }
+
     private static class EditForSaleConfigResponse {
         ForSaleConfig forSaleConfig;
     }
