662ad3323a7b458e8f0b0fdd472c616a8c3db112
mroloux <matti@seats.io>
2026-05-22 12:53:39 +0200

    Add areaPartitionLabel method to Channel and create corresponding tests

--- src/main/java/seatsio/events/Channel.java
+++ src/main/java/seatsio/events/Channel.java
@@ -4,4 +4,8 @@ import java.util.Map;
 import java.util.Set;
 
 public record Channel(String key, String id, String name, String color, Integer index, Set<String> objects, Map<String, Integer> areaPlaces) {
+
+    public String areaPartitionLabel(String areaLabel) {
+        return areaLabel + "##" + id;
+    }
 }
