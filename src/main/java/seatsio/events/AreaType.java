455ea30cc0c202d6cca40112a3fe8efb0b8a6a3f
mroloux <matti@seats.io>
2026-09-16 15:17:07 +0200

    Remove comment

--- src/main/java/seatsio/events/AreaType.java
+++ src/main/java/seatsio/events/AreaType.java
@@ -1,9 +1,5 @@
 package seatsio.events;
 
-/**
- * Not a Java enum, for forward compatibility: the backend may return area types that this
- * version of the library doesn't know about yet.
- */
 public final class AreaType {
 
     public static final String GENERAL_ADMISSION = "generalAdmission";
